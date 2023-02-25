967c6be37b152e24cbe31184b605d4cc4764d5e1
noviceyash <noviceyash21@gmail.com>
2023-02-25 15:31:39 +0530

    PixelOS Bringup
    Signed-off-by: noviceyash <noviceyash21@gmail.com>

--- aosp_onclite.mk
+++ aosp_onclite.mk
@@ -12,12 +12,10 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_p.mk)
 # Inherit from onclite device
 $(call inherit-product, device/xiaomi/onclite/device.mk)
 
-# Inherit some common ProjectBlaze stuff.
-$(call inherit-product, vendor/blaze/config/common_full_phone.mk)
+# Inherit some common PixelOS stuff.
+$(call inherit-product, vendor/aosp/config/common_full_phone.mk)
 
-# Blaze Official Stuff
-BLAZE_BUILD_TYPE := OFFICIAL
-BLAZE_MAINTAINER := ＡＤＩＴＹＡ
+# Pixel OS Stuff
 TARGET_FACE_UNLOCK_SUPPORTED := true
 TARGET_USE_PIXEL_CHARGER := true
 TARGET_SUPPORTS_BLUR := true
@@ -26,7 +24,7 @@ TARGET_SUPPORTS_BLUR := true
 TARGET_BOOT_ANIMATION_RES := 720
 
 # Device identifier. This must come after all inclusions.
-PRODUCT_NAME := blaze_onclite
+PRODUCT_NAME := aosp_onclite
 PRODUCT_DEVICE := onclite
 PRODUCT_BRAND := Xiaomi
 PRODUCT_MODEL := Redmi 7
